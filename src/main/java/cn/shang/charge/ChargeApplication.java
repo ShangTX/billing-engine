35ae645db5d5dfd553834566ae85aea5d1a71bc6
shangtx <shangtx@126.com>
2025-12-01 09:14:33 +0800

    chore: 初始化

---
+++ src/main/java/cn/shang/charge/ChargeApplication.java
@@ -0,0 +1,13 @@
+package cn.shang.charge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ChargeApplication.class, args);
+    }
+
+}
